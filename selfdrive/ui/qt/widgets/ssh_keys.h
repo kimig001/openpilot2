5bce4011030de9b8020dd3950aad9b1ab32c397e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2021-10-30 21:38:05 +0900

    Update ssh_keys.h

--- selfdrive/ui/qt/widgets/ssh_keys.h
+++ selfdrive/ui/qt/widgets/ssh_keys.h
@@ -33,18 +33,6 @@ private:
   void getUserKeys(const QString &username);
 };
 
-class PrebuiltToggle : public ToggleControl {
-  Q_OBJECT
-
-public:
-  PrebuiltToggle() : ToggleControl("Prebuilt 파일 생성", "Prebuilt 파일을 생성하며 부팅속도를 단축시킵니다. UI수정을 한 경우 기능을 끄십시오.", "../assets/offroad/icon_shell.png", Params().getBool("PutPrebuiltOn")) {
-    QObject::connect(this, &PrebuiltToggle::toggleFlipped, [=](int state) {
-      char value = state ? '1' : '0';
-      Params().put("PutPrebuiltOn", &value, 1);
-    });
-  }
-};
-
 // LateralControlSelect
 class LateralControlSelect : public AbstractControl {
   Q_OBJECT
